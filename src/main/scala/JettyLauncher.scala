5bc8e5e10baba684f2d7afd55e9110d6533810c7
Olivier Bazoud <olivier.bazoud@gmail.com>
2013-01-13 22:32:44 +0100

    Heroku deployment

---
+++ src/main/scala/JettyLauncher.scala
@@ -0,0 +1,28 @@
+/*
+ * Starts jetty for scalatra programatically
+ *
+ * Replace YourApplicationEndpointFilter with the filter in your application
+ */
+
+import com.bazoud.scalastory.ScalaStoryServlet
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
+
+/**
+ * @author @obazoud (Olivier Bazoud)
+ */
+object JettyLauncher {
+  def main(args: Array[String]) {
+    val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
+
+    val server = new Server(port)
+    val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
+
+    context.addServlet(classOf[ScalaStoryServlet], "/");
+    context.setResourceBase("src/main/webapp")
+
+    server.start
+    server.join
+  }
+
+}
