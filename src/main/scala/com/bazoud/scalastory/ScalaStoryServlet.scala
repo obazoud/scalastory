a1ab6ebb13d787371c2768a5f2c5391a8b369ce4
Olivier Bazoud <olivier.bazoud@gmail.com>
2013-01-16 14:23:34 +0100

    happy to play

--- src/main/scala/com/bazoud/scalastory/ScalaStoryServlet.scala
+++ src/main/scala/com/bazoud/scalastory/ScalaStoryServlet.scala
@@ -9,6 +9,7 @@ class ScalaStoryServlet extends ScalatraServlet with ScalateSupport {
     try {
       params("q") match {
         case "Quelle est ton adresse email" => Ok("olivier.bazoud@gmail.com")
+        case "Es tu heureux de participer(OUI/NON)" => Ok("OUI")
         case _   => BadRequest("oops: " + params("q"))
       }
     } catch {
--- src/test/scala/com/bazoud/scalastory/ScalaStorySeervletSpec.scala
+++ src/test/scala/com/bazoud/scalastory/ScalaStorySeervletSpec.scala
@@ -3,23 +3,41 @@ package com.bazoud.scalastory
 import org.scalatra.test.specs2._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
-class ScalaStorySeervletSpec extends ScalatraSpec { def is =
-  "GET / on ScalaStorySeervlet"                     ^
-    "should return status 200"                  ! root200^
-                                                end
+class ScalaStorySeervletSpec extends MutableScalatraSpec {
 
   addServlet(classOf[ScalaStoryServlet], "/*")
 
-  def root200 = get("/") {
-    status must_== 200
+  "GET / on ScalaStoryServlet" should {
+    "return status 200" in {
+      get("/") {
+        status must_== 200
+      }
+    }
   }
 
-  def query_q_mail = get("/?q=Quelle est ton adresse email") {
-    status must_== 200
-    response must_== "olivier.bazoud@gmail.com"
+  "GET /?q=Quelle+est+ton+adresse+email on ScalaStoryServlet" should {
+    "return status 200" in {
+      get("/?q=Quelle+est+ton+adresse+email") {
+        status must_== 200
+        body must_== "olivier.bazoud@gmail.com"
+      }
+    }
   }
 
-  def query_q_wtf = get("/?q=wtf") {
-    status must_== 403
+  "GET /?q=wtf on ScalaStoryServlet" should {
+    "return status 400" in {
+      get("/?q=wtf") {
+        status must_== 400
+      }
+    }
+  }
+
+  "GET /?q=Es+tu+heureux+de+participer(OUI/NON) on ScalaStoryServlet" should {
+    "return status 200" in {
+      get("/?q=Es+tu+heureux+de+participer(OUI/NON)") {
+        status must_== 200
+        body must_== "OUI"
+      }
+    }
   }
 }
